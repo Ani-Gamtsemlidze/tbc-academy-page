4b8a6db06e459a96b98276c12a0872a7823e3976
User-s <ani.gamcemlidze20@gmail.com>
2024-01-22 21:21:26 +0400

    add functionality in slider section

--- scripts/slider/slider.js
+++ scripts/slider/slider.js
@@ -1,44 +1,81 @@
+// select elements and store in variables
 const sliderImages = document.querySelectorAll(".slide");
+
 const arrowPrev = document.getElementById("prev-arrow");
 const arrowNext = document.getElementById("next-arrow");
 
-let current = 0;
+const bulletsContainer = document.querySelector(".slider-bullets");
 
-const reset = () => {
+let currentIndex = 0;
+let slideInterval;
+
+// this function sets slide images hide
+
+function hideAllSlides() {
   for (const image of sliderImages) {
     image.style.display = "none";
   }
-};
+}
 
-const init = () => {
-  reset();
-  sliderImages[0].style.display = "block";
-};
+// // this function creates bullet dots in the slider HTML
+function createBullets() {
+  sliderImages.forEach((_, index) => {
+    const bullet = document.createElement("span");
+    bullet.classList.add("bullet");
+    bullet.addEventListener("click", () => goToSlide(index));
+    bulletsContainer.appendChild(bullet);
+  });
+  updateBullets();
+}
 
-const slidePrev = () => {
-  reset();
-  sliderImages[current - 1].style.display = "block";
-  current--;
-};
+// initialization function to set up the initial state of the slider
+function init() {
+  hideAllSlides();
+  sliderImages[0].style.display = "block"; // show only first slide page at first
+  createBullets();
+}
 
+function updateBullets() {
+  const bullets = document.querySelectorAll(".bullet");
+  // loop through each bullet element using forEach
+
+  bullets.forEach((bullet, index) => {
+    // use classList.toggle to add or remove the "active" class based on the condition (index === currentIndex)
+    bullet.classList.toggle("active", index === currentIndex);
+  });
+}
+
+// navigate to a specific slide by its index
+function goToSlide(index) {
+  hideAllSlides();
+  sliderImages[index].style.display = "block";
+  // update the current index
+  currentIndex = index;
+  updateBullets();
+}
+
+//
 arrowPrev.addEventListener("click", () => {
-  if (current === 0) {
-    current = sliderImages.length;
-  }
-  slidePrev();
+  currentIndex = (currentIndex - 1 + sliderImages.length) % sliderImages.length;
+  console.log(currentIndex);
+  goToSlide(currentIndex);
 });
-
-const slideRight = () => {
-  reset();
-  sliderImages[current + 1].style.display = "block";
-  current++;
-};
 
 arrowNext.addEventListener("click", () => {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
-  slideRight();
+  currentIndex = (currentIndex + 1) % sliderImages.length;
+  goToSlide(currentIndex);
 });
 
+function startAutoSlide(duration) {
+  slideInterval = setInterval(() => {
+    currentIndex = (currentIndex + 1) % sliderImages.length;
+    goToSlide(currentIndex);
+  }, duration);
+}
+
+function stopAutoSlide() {
+  clearInterval(slideInterval);
+}
+
 init();
+startAutoSlide(2000);
